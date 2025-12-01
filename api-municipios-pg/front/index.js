a3a75445899a1add20973c0c1893871e0c1cacdd
rmoraes-git <contatoimediato@gmail.com>
2025-12-01 15:41:34 -0300

    add front

---
+++ api-municipios-pg/front/index.js
@@ -0,0 +1,47 @@
+const API = "http://127.0.0.1:3000/municipios";
+const listagem = document.getElementById("listagem");
+console.log('hhhhhhh')
+
+const btnCarregar = document.getElementById("btn");
+btnCarregar.addEventListener("click", carregarMunicipios)
+
+const btnSalvar = document.getElementById("btnSalvar");
+btnSalvar.addEventListener("click", inserirMunicipio)
+
+async function carregarMunicipios() {
+    console.log("teste");
+    try {
+        //fetch
+        const resposta = await fetch(API);
+        const dados = await resposta.json();
+        listagem.innerHTML = "";
+        dados.forEach(municipio => {
+            const linha = document.createElement("li");
+            linha.textContent = municipio.nome + " " + municipio.caracteristica;
+            listagem.appendChild(linha)
+        });
+
+    }
+    catch (erro) {
+        console.log("errorrrrrrrrrrrrr", erro.message)
+    }
+}
+
+async function inserirMunicipio() {
+    const novoMunicipio = {
+        nome: "novo municipio",
+        estado: "RS",
+        caracteristica: "cidade das flores"
+    };
+
+    try {
+        const resposta = await fetch(API, {
+            method: "POST",
+            body: JSON.stringify(novoMunicipio),
+            headers: { "Content-Type": "application/json" }
+        })
+
+    } catch (erro) {
+        console.error("deu ruim ", erro.message)
+    }
+}
