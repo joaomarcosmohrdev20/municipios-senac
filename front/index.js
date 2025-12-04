6b9786d6a6a4897474c637e716e0ef7361a544d9
joaomarcosmohrdev20 <joaomarcosmohr20@gmail.com>
2025-12-03 21:48:34 -0300

    tudo

--- front/index.js
+++ front/index.js
@@ -4,6 +4,9 @@ const listagem = document.getElementById("listagem");
 const btnCarregar = document.getElementById("btn");
 const btnSalvar = document.getElementById("btnSalvar");
 
+const campoMunicipio = document.getElementById("campoMunicipio")
+const campoUF = document.getElementById("campoUF")
+const campoCaracteristica = document.getElementById("campoCaracteristica")
 // Eventos
 btnCarregar.addEventListener("click", carregarMunicipios);
 btnSalvar.addEventListener("click", inserirMunicipio);
@@ -19,6 +22,7 @@ async function carregarMunicipios() {
         listagem.innerHTML = ""; // limpa
 
         dados.forEach(m => criarCard(m));
+        
 
     } catch (erro) {
         console.error("Erro ao carregar:", erro.message);
@@ -29,16 +33,20 @@ async function carregarMunicipios() {
 // CRIAR CARD NO FRONT
 //--------------------------------------------------
 function criarCard(m) {
-    const card = document.createElement("div");
-    card.classList.add("card");
 
-    card.innerHTML = `
-        <h3>${m.nome} (${m.estado})</h3>
-        <p>${m.caracteristica}</p>
-        <button class="btn-delete" onclick="deletar()">Deletar</button>
-    `;
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-    listagem.appendChild(card);
+        card.innerHTML = `
+            <h3>${m.nome} (${m.estado})</h3>
+            <p>${m.caracteristica}</p>
+            <button class="btn-delete" onclick="deletar(${m.id})">Deletar</button>
+            <button class="btn-alterar" onclick="alterarMunicipio(${m.id})>Alterar</button>
+        `;
+
+        listagem.appendChild(card);
+        alterarMunicipio();
+        carregarMunicipios();
 }
 
 //--------------------------------------------------
@@ -64,11 +72,35 @@ async function inserirMunicipio() {
 
         carregarMunicipios();
 
+        document.getElementById("campoMunicipio").value = "";
+        document.getElementById("campoUF").value = "";
+        document.getElementById("campo").value = "";
+
     } catch (erro) {
         console.error("Erro ao inserir:", erro.message);
     }
 }
 
-async function deletar(){
-    alert("vou deletar");
+async function deletar(idDeletar){
+    
+    try{
+        const urlDelete = API+"/"+idDeletar
+        const resposta = await fetch(urlDelete,
+            {
+                method: "DELETE"
+            }
+        )
+
+        carregarMunicipios();
+    }
+    catch {
+        console.log("error")
+    }
+}
+
+async function alterarMunicipio(id) {
+    document.getElementById("campoMunicipio").value = id;
+    document.getElementById("campoUF").value = id;
+    document.getElementById("campo").value = id;
+
 }
